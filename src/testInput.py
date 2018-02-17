1e165584bcf47f4996ca9fd01aab8f6aa44ed82e
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-02-17 14:44:32 +0100

    initParsing

--- src/testInput.py
+++ src/testInput.py
@@ -34,6 +34,46 @@ False
 """
 
 
+import alphabet_def
+
+var = alphabet_def.VAR_SET
+op = alphabet_def.POSSIBLE_OP
+dot = alphabet_def.LAMBDA_DOT
+
+
+
+
+
+
+
+
+def initParsing(candidate):
+    """
+    Initialize the test.
+
+    :param candidate: the string to parse
+    :type candidate: str
+    :return: true if candidate is well formed, false otherwise
+    :rtype: bool
+    """
+    if type(candidate) == str and candidate in var:
+        return True
+
+    elif type(candidate) != str\
+       or len(candidate) < 4\
+       or candidate[0] != '('\
+       or not set(candidate).issubset(alphabet_def.TOTAL_ALPHABET):
+        return False
+
+    else:
+        return parse(candidate, 0)
+
+
+
+
+
+
+
 
 
 
